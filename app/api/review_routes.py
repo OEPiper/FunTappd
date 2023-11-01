04e6709b81d53fb2c4f3328ef6e02400045f658d
Matt Piper <oepiper@gmail.com>
2023-11-01 10:22:49 -0400

    review backend

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -43,3 +43,28 @@ def create_review():
         return jsonify(new_review.to_dict())
     return {'error'}
 
+@review_bp.route('/<int:id>/update', methods=['PUT'])
+def update_review(id):
+    review_to_update = Review.query.get(id)
+    if review_to_update is None:
+        return "Review not found", 404
+    update_data = request.get_json()
+    if "rating" in update_data:
+        review_to_update.rating = update_data["rating"]
+    if "text" in update_data:
+        review_to_update.text = update_data["text"]
+    if "user_id" in update_data:
+        review_to_update.user_id = update_data["user_id"]
+    if "beer_id" in update_data:
+        review_to_update.beer_id = update_data["beer_id"]
+    db.session.commit()
+    return review_to_update.to_dict()
+
+@review_bp.route('/<int:id>/delete', methods=['DELETE'])
+def delete_review(id):
+    review_to_remove = Review.query.get(id)
+    if review_to_remove is None:
+        return "Review not Found", 404
+    db.session.delete(review_to_remove)
+    db.session.commit()
+    return "Review has been removed"
